8717444f5dcd103b390f85c4fb7cb0a90e1000a7
NikNik2023 <novitskiy_nn@mail.ru>
2024-07-06 16:29:36 +0300

    Add table

---
+++ db/migrate/20240706130019_add_products.rb
@@ -0,0 +1,27 @@
+class AddProducts < ActiveRecord::Migration[7.1]
+  def change
+    Product.create :title => 'Hawaiian',
+                   :description => 'This is Hawaiian pizza',
+                   :price => 350,
+                   :size => 30,
+                   :is_spicy => false,
+                   :is_veg => false,
+                   :path_to_image => '/images/hawaiian.jpg'
+
+    Product.create :title => 'Pepperoni',
+                   :description => 'Nice Pepperoni pizza',
+                   :price => 450,
+                   :size => 30,
+                   :is_spicy => false,
+                   :is_veg => false,
+                   :path_to_image => '/images/pepperoni.jpg'
+
+    Product.create :title => 'Vegeterian',
+                   :description => 'This is Vegeterian pizza',
+                   :price => 400,
+                   :size => 30,
+                   :is_spicy => false,
+                   :is_veg => true,
+                   :path_to_image => '/images/veg.jpg'
+  end
+end
